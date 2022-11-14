a126085b07ff39c587ba9d77dae8202f092c62a9
ellamarym <ellamuir@icloud.com>
2022-11-14 14:52:08 +0000

    modified app to remove usage of express.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { getTopics } = require("./controllers/app.controller");
 const app = express();
-app.use(express.json());
+
 
 app.get('/api/topics', getTopics )
 
