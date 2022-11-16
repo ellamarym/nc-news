e5272e2302ec6d2fa3bbc4bff2d8aac41fe1c0bb
ellamarym <ellamuir@icloud.com>
2022-11-16 11:00:25 +0000

    changes made to ticket 9

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -77,5 +77,7 @@ exports.patchArticleById = (req,res,next) => {
 exports.getUsers = (req, res, next) => {
     fetchUsers().then((users) => {
         res.status(200).send({users})
+    }).catch((err)=> {
+        next(err)
     })
 }
